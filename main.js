d15cf62385c050b60cff5faf5c35989cf049261b
sulaymonkhon1238 <sulaymonkhon2222@gmail.com>
2023-02-12 10:49:25 +0500

    tasbex

---
+++ main.js
@@ -0,0 +1,33 @@
+// function loop(n) {
+//   for (let i = 100; i <= n; i++);
+// }
+//  console.log(loop(n))
+// function loop(n){
+//     let counter = 0;
+//     for (let i = 100; i <= n; i+=45){
+//         if (i % 45 ===0){
+//             counter++;
+//         }
+//     }
+// }
+// loop(999)
+// function loop(arr){
+//     let lastIndex = arr.length ;
+//   console.log(arr[lastIndex]);
+// }
+// let arr = ['abdulboriy', 'fayoz', 'sulaymon', 'nurullo'];
+
+// loop(arr)
+// document.querySelector('.sanoq').addEventListener('click', function(){
+// var sanoq = document.querySelector('.display');
+// console.log(typeof  parseInt(ekran.value));
+// sanoq.value = parseInt(sanoq.value) + 1;
+// });
+// document.querySelector('.reset').addEventListener('click', function(){
+// document.querySelector('.display').value = 0;
+// }
+// )
+
+
+
+
